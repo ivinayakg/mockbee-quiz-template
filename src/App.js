1ee9ca65b056959615f9544a091f07ca0befe226
Nitin kalra <nikkalra88@gmail.com>
2022-05-30 15:56:36 +0530

    fix: updated dependencies

--- src/App.js
+++ src/App.js
@@ -3,43 +3,7 @@ import logo from "./logo.png";
 import Mockman from "mockman-js";
 
 function App() {
-  return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} alt="mockBee logo" width="180" height="180" />
-        <h1 className="brand-title">
-          Welcome to <span>mockBee!</span>
-        </h1>
-        <p className="brand-description">
-          Get started by editing <code>src/App.js</code>
-        </p>
-        <div className="links">
-          <a
-            href="https://mockbee.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Explore mockBee
-          </a>
-          <a
-            href="https://mockbee.netlify.app/docs/api/introduction"
-            target="_blank"
-            rel="noreferrer"
-          >
-            API Documentation
-          </a>
-          <a
-            href="https://github.com/neogcamp/mockBee"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Contribute
-          </a>
-        </div>
-      </header> */}
-      <Mockman colorScheme="dark" />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
