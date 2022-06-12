53639fa678d3df7dc76743047f157c0ae8106fe1
Harshita Daswani <87665916+harshitadaswani@users.noreply.github.com>
2022-06-12 22:13:13 +0530

    feat: screens
    added static UI screens

---
+++ src/components/shared/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <>
+      <div class="p-s txt-center m-auto footer">
+        <p>
+          Copyright <span class="fw-semibold">©</span> Crypto | Designed by{" "}
+          <span class="fw-semibold action-color">Harshita Daswani</span>
+        </p>
+      </div>
+    </>
+  );
+};
