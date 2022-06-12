8e60e172891f5ae1a732820eb4a457b94ab0fe9a
Harshita Daswani <87665916+harshitadaswani@users.noreply.github.com>
2022-06-12 22:13:49 +0530

    Revert "feat: screens"
    This reverts commit 53639fa678d3df7dc76743047f157c0ae8106fe1.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
 import "./App.css";
-import { Footer } from "./components";
-import { LandingPage } from "./pages";
-// import Mockman from "mockman-js";
+import logo from "./logo.png";
+import Mockman from "mockman-js";
 
 function App() {
   return (
-    <div className="pagewrap">
-      <LandingPage />
-      <Footer />
-      {/* <Mockman colorScheme="dark" /> */}
+    <div className="App">
+      {/* <header className="App-header">
+        <img src={logo} alt="mockBee logo" width="180" height="180" />
+        <h1 className="brand-title">
+          Welcome to <span>mockBee!</span>
+        </h1>
+        <p className="brand-description">
+          Get started by editing <code>src/App.js</code>
+        </p>
+        <div className="links">
+          <a
+            href="https://mockbee.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Explore mockBee
+          </a>
+          <a
+            href="https://mockbee.netlify.app/docs/api/introduction"
+            target="_blank"
+            rel="noreferrer"
+          >
+            API Documentation
+          </a>
+          <a
+            href="https://github.com/neogcamp/mockBee"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Contribute
+          </a>
+        </div>
+      </header> */}
+      <Mockman colorScheme="dark" />
     </div>
   );
 }
